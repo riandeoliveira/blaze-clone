0ba1b472c021bdef1eb43935a46874fd900ba7fb
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-09-10 23:43:04 -0300

    feat: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import { ReactComponent as BlazeLogo } from "./icons/blaze-logo.svg";
+import { ReactComponent as BRL } from "./icons/brl-icon.svg";
+import { ReactComponent as Cassino } from "./icons/cassino-icon.svg";
+import { ReactComponent as Notifications } from "./icons/notifications-icon.svg";
+import { ReactComponent as Profile } from "./icons/profile-icon.svg";
+import { ReactComponent as Sports } from "./icons/sports-icon.svg";
+
+export const Icon = {
+  BlazeLogo,
+  BRL,
+  Cassino,
+  Notifications,
+  Profile,
+  Sports,
+};
