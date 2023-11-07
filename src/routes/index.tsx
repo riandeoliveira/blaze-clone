a6a76332bae7b41bb5e4239a4662283240e45e2b
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-11-07 00:05:55 -0300

    feat: redirect to crash game page when accessing root page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
 import { Games } from "features/games/pages";
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { AppRoute } from "./AppRoute";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/games/crash" />,
+  },
   {
     path: "/games/crash",
     element: <AppRoute page={Games.Crash} />,
