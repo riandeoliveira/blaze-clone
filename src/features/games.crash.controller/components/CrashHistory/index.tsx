31bf534c13932caadb2e224ffe76dd1d66e34e2f
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-11-08 00:07:02 -0300

    feat: add crash history animations

--- src/features/games.crash.controller/components/CrashHistory/index.tsx
+++ src/features/games.crash.controller/components/CrashHistory/index.tsx
@@ -1,3 +1,4 @@
+import { statusStore } from "features/games.crash.screen/stores/status.store";
 import { observer } from "mobx-react-lite";
 import type { ReactElement } from "react";
 import { localStorageStore } from "store/local-storage.store";
@@ -10,7 +11,12 @@ export const CrashHistory = observer((): ReactElement => {
       <div className={styles.list_container}>
         <div className={styles.list}>
           {localStorageStore.crashHistory.map((crashPoint, index) => (
-            <span className={styles.crash_point} data-crash-good={crashPoint >= 2} key={index}>
+            <span
+              className={styles.crash_point}
+              data-crashed={statusStore.isCrashed}
+              data-crash-good={crashPoint >= 2}
+              key={index}
+            >
               {crashPoint.toFixed(2).replace(".", ",")}X
             </span>
           ))}
