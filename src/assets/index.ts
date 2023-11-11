c00b5feb848b4ebdcaac83273ea9b6327dcd15b5
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-11-10 21:23:26 -0300

    feat: add blaze mobile logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
-import { ReactComponent as BlazeLogo } from "./icons/blaze-logo.svg";
+import { ReactComponent as BlazeDesktopLogo } from "./icons/blaze-desktop-logo.svg";
+import { ReactComponent as BlazeMobileLogo } from "./icons/blaze-mobile-logo.svg";
 import { ReactComponent as BRL } from "./icons/brl-icon.svg";
 import { ReactComponent as Cassino } from "./icons/cassino-icon.svg";
 import { ReactComponent as Check } from "./icons/check-icon.svg";
@@ -6,7 +7,8 @@ import { ReactComponent as CrashRocket } from "./icons/crash-rocket.svg";
 import { ReactComponent as Sports } from "./icons/sports-icon.svg";
 
 export const Icon = {
-  BlazeLogo,
+  BlazeDesktopLogo,
+  BlazeMobileLogo,
   BRL,
   Cassino,
   Check,
